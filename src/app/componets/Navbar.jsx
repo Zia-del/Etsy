94938120649f8f469f0dac7763252160ac21d0c5
Zia <prettychinonso963>
2024-09-19 12:46:49 -0700

    Add navbar

---
+++ src/app/componets/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className='nav1'>
+        <h1 className='h1'>Etsy</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
